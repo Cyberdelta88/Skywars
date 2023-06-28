c0176c01f305f214175a26247a4aef7599b19f93
cyberdelta88 <cyberdelta88@gmail.com>
2023-06-28 16:10:59 +0200

    Initial commit

--- src/main/java/fr/cyberdelta88/skywars/Main.java
+++ src/main/java/fr/cyberdelta88/skywars/Main.java
@@ -1,2 +1,18 @@
-package fr.cyberdelta88.skywars;public class Main {
+package fr.cyberdelta88.skywars;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        Bukkit.getConsoleSender().sendMessage(ChatColor.DARK_PURPLE + "The skywars plugin was succesfully launched \n" +
+                ChatColor.LIGHT_PURPLE + "Thank you for using this plugin");
+    }
+
+    @Override
+    public void onDisable() {
+        //on disable
+    }
 }
